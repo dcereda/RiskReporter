15393aa53f3c27dcd4dda94f1f23f5013836978d
Danilo Cereda <danilo.cereda@gmail.com>
2015-12-28 14:28:17 -0200

    Yo Angular install validated
    Project validated

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name riskReporterApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the riskReporterApp
+ */
+angular.module('riskReporterApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
